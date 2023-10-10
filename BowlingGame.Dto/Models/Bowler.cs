04a028cba690a43134de7a436bad825b32b958c7
David Rodrigues <davidrodrigues40@gmail.com>
2023-10-10 15:29:45 -0400

    Refactored bowler and player

--- BowlingGame.Dto/Models/Bowler.cs
+++ BowlingGame.Dto/Models/Bowler.cs
@@ -1,13 +1,8 @@
 ﻿using BowlingGame.Core.Abstractions.Models;
-using BowlingGame.Core.Enums;
 
 namespace BowlingGame.Dto.Models;
-public record Bowler : IBowler
+public record Bowler : Player, IBowler
 {
-    public string Name { get; set; } = string.Empty;
     public Dictionary<int, IFrame> Frames { get; set; } = new();
     public int Score { get; set; } = 0;
-    public BowlerRating Rating { get; set; } = BowlerRating.Beginner;
-
-    public Bowler() { }
 }
